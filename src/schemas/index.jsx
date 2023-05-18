e504eeca635af2ed8b33783cf3e063f9aabdaea1
codewithamans <a.jagat159753@gmail.com>
2023-05-18 13:39:35 +0530

    few files deleted

--- src/schemas/index.jsx
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-export const userSchema = yup.object().shape({
-  name: yup.string().min(5).max(30).required("Enter your Name"),
-  email: yup.string().email().required("Enter your Email"),
-  mobile: yup.number().max(10).required("Enter your Mobile Number"),
-});
